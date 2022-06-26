507aa3b6036c1d6ce0a9e9a6323b1373fe509b27
David Bobeck <bobeckd@gmail.com>
2022-06-26 12:55:22 -0400

    first async turbo frame

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,15 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def async_turbo_frame
+    # binding.irb
+    # render html: "<h1.text-center>This is cool</h1>"
+    # render :async_turbo_frame, status: :ok
+
+    # respond_to do |format|
+    #   format.html
+    # end
+    sleep(2)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   get 'pages/home'
   get 'pages/about'
+  get 'pages/async_turbo_frame'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
